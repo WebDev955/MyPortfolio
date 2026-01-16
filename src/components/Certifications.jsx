1bd5c22de3f53b1e81853d191e91bfc3ead0bf8f
WebDev955 <austingeisinger955@gmail.com>
2026-01-16 13:19:38 -0500

    fixed certification links

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -3,14 +3,16 @@ import { useRef, forwardRef } from "react";
 
 //Import - styles
 import styles from "./Certifications.module.css"
+import JavaScriptCert from "../../public/Javascript.pdf"
+import ResponsiveWebCrt from "../../public/ResponsiveWebDesign.pdf"
 
 
 const Certifications = forwardRef ((props, ref) => {
     return (
         <div ref={ref} className={styles.certMainWrapper}>
             <h1>Certifications</h1>
-            <p><a href="public\FreeCodeCampCert-JavasScript.pdf">FreeCodeCamp - Javascript</a></p> <br/>
-            <p><a href="public\FreeCodeCampCert-ResponsiveWebDesign.pdf">FreeCodeCamp - HTML & CSS</a></p><br/>
+            <p><a href={JavaScriptCert}>FreeCodeCamp - Javascript</a></p> <br/>
+            <p><a href={ResponsiveWebCrt}>FreeCodeCamp - HTML & CSS</a></p><br/>
             <p> udemy - React "The Compplete Guide 2025" by Maximilian Schwarzmüller</p>
             <p>(In Active Progress - Core understanding of React completed)</p>
         </div>
